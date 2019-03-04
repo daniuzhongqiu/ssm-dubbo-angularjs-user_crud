da23d57e52ba351bff9d736eb9a11a6c8a5b30cf
zhongqiu <1595948168@qq.com>
2019-03-04 17:12:14 +0800

    user_curd 练习

---
+++ user_parent/user_service/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -0,0 +1,106 @@
+package com.itheima.service.impl;
+import java.util.List;
+
+import com.itheima.mapper.TbUserMapper;
+import com.itheima.pojo.PageResult;
+import com.itheima.pojo.TbUserExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.itheima.pojo.TbUser;
+import com.itheima.service.UserService;
+
+
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private TbUserMapper userMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbUser> findAll() {
+		return userMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbUser> page= (Page<TbUser>) userMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(TbUser user) {
+		userMapper.insert(user);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbUser user){
+		userMapper.updateByPrimaryKey(user);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbUser findOne(Integer id){
+		return userMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Integer[] ids) {
+		for(Integer id:ids){
+			userMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbUser user, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbUserExample example=new TbUserExample();
+		TbUserExample.Criteria criteria = example.createCriteria();
+		
+		if(user!=null){			
+						if(user.getUsername()!=null && user.getUsername().length()>0){
+				criteria.andUsernameLike("%"+user.getUsername()+"%");
+			}
+			if(user.getPassword()!=null && user.getPassword().length()>0){
+				criteria.andPasswordLike("%"+user.getPassword()+"%");
+			}
+			if(user.getType()!=null && user.getType().length()>0){
+				criteria.andTypeLike("%"+user.getType()+"%");
+			}
+	
+		}
+		
+		Page<TbUser> page= (Page<TbUser>)userMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+	
+}
